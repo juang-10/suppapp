f2ed1095d0b214c85c2433b361fcb446c5705587
Juan <[judagosa8520@gmail.com]>
2022-05-02 22:09:28 -0500

    Conexion con el rol de usuario

--- models/Usuario.php
+++ models/Usuario.php
@@ -2,7 +2,7 @@
     class Usuario extends Conectar {
         public function login() {
             $conectar=parent::conexion();
-            parent::set_name();
+            parent::set_names();
             if(isset($_POST["enviar"])) {
                 $correo = $_POST["usu_correo"];
                 $pass = $_POST["usu_pass"];
